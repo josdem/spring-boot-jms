e5838edc9b8266324e44ae953145899387e8ff5e
josdem <joseluis.delacruz@gmail.com>
2018-02-25 16:15:28 -0600

    Adding jms send message basement

--- src/main/groovy/com/jos/dem/springboot/jms/controller/DemoController.groovy
+++ src/main/groovy/com/jos/dem/springboot/jms/controller/DemoController.groovy
@@ -3,11 +3,16 @@ package com.jos.dem.springboot.jms.controller
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 
+import com.jos.dem.springboot.jms.command.Command
+import com.jos.dem.springboot.jms.command.PersonCommand
+
 @RestController
 class DemoController {
 
   @RequestMapping('/')
   String index(){
+  	Command person = new PersonCommand(nickname:'josdem', email:'joseluis.delacruz@gmail.com')
+  	
   	'Hello World!'
   }
 	
