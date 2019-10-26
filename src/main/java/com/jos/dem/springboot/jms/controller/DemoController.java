5688e7348058f398c3ba87737ccbfba78423ab08
josdem <joseluis.delacruz@gmail.com>
2019-10-26 13:50:17 -0400

    Adding log into index controller

--- src/main/java/com/jos/dem/springboot/jms/controller/DemoController.java
+++ src/main/java/com/jos/dem/springboot/jms/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.jos.dem.springboot.jms.controller;
 
+import reactor.core.publisher.Mono;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +10,23 @@ import com.jos.dem.springboot.jms.command.Command;
 import com.jos.dem.springboot.jms.command.PersonCommand;
 import com.jos.dem.springboot.jms.service.MessageService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class DemoController {
 
 	@Autowired
   private MessageService messageService;
 
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
   @GetMapping("/")
-  public String index(){
+  public Mono<String> index(){
+    log.info("Sending message");
   	Command person = new PersonCommand("josdem","joseluis.delacruz@gmail.com");
   	messageService.sendMessage(person);
-  	return "Java Message Service";
+  	return Mono.just("Java Message Service");
   }
 
 }
